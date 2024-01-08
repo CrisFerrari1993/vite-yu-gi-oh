ff7b2e91ffa03a5a1b693ab53ec8969f2c2e542f
Cristopher Ferrari <ferraricristopher93@gmail.com>
2024-01-08 16:26:33 +0100

    AppHeader : Done

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    name: 'AppHeader'
+}
+</script>
+
+<template>
+    <header class="container-fluid">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/1/11/Yu-Gi-Oh%21_%28Logo%29.jpg" alt="">
+        <h1>Yu-Gi-Oh Api</h1>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+header {
+    background-color: white;
+    display: flex;
+    align-items: center;
+
+    img {
+        width: 50px;
+    }
+}
+</style>
