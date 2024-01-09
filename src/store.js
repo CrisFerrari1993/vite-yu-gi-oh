283859cffa58568911fc5e25cdef4cc602ba2367
Cristopher Ferrari <ferraricristopher93@gmail.com>
2024-01-09 17:06:19 +0100

    aggiunta url archetipi

--- src/store.js
+++ src/store.js
@@ -2,7 +2,10 @@ import { reactive } from "vue";
 
 export const store = reactive({
     cardList: [],
-    apiURL: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0',
+    apiURL: "https://db.ygoprodeck.com/api/v7/cardinfo.php",
+    apiArchURL: "https://db.ygoprodeck.com/api/v7/archetypes.php",
+    archList: [],
+    archSelect: "",
     searchText: '',
     loading: true,
 });
