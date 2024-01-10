ab57b723ae56da5a9e70264e898d4681c2b45477
Cristopher Ferrari <ferraricristopher93@gmail.com>
2024-01-10 09:50:47 +0100

    Cancellazzione di epoerimenti

--- src/components/DropDown.vue
+++ src/components/DropDown.vue
@@ -9,39 +9,11 @@ export default {
             emptyString: "",
         }
     },
-    methods: {
-        // archSelectFunc(string) {
-        //     store.archSelect = string;
-        // }
-    },
 }
 
 </script>
 
 <template>
-    <!-- MODEL FROM BOOTSTRAP -->
-
-    <!-- need to uncomment:
-        - methods: archSelectFunc(string)
-        - style: .dropdown-menu{} -->
-
-    <!-- <div class="dropdown">
-        <button class="btn btn-secondary btn-sm dropdown-toggle my-3 bg-white text-black" type="button"
-            data-bs-toggle="dropdown" aria-expanded="false">
-            Select archetype...
-        </button>
-        <ul class="dropdown-menu">
-            <li @click="archSelectFunc(this.emptyString)" @click.prevent="$emit('archSelected')"><a class=" dropdown-item"
-                    href="#">All</a>
-            </li>
-            <li v-for="(item, idx) in store.archList" :key="idx" @click="archSelectFunc(item.archetype_name)"
-                @click.prevent="$emit('archSelected')">
-                <a class=" dropdown-item" href="#">{{ item.archetype_name
-                }}</a>
-            </li>
-        </ul>
-    </div> -->
-
     <!-- SELECT MENU -->
     <select name="archetypes" id="archetype-select" v-model="store.archSelect" @change="$emit('archSelected')"
         class="btn btn-secondary btn-sm dropdown-toggle my-3 bg-white text-black">
